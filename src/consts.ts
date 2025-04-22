0d68f83d7430b5c61f45f754a382422b719aebd2
Lui <lsanch78@asu.edu>
2025-04-22 11:49:39 -0700

    Updated site header

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Gargylon Records";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Home of Gargylon Records";
